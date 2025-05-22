d2efc593cd2cbdc6bafd61a82b1a443adb142b13
Tretyakov Maksim <120916702+Shutcing@users.noreply.github.com>
2025-05-23 01:10:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const logoWidth = `${document.querySelector(".header__logo").clientWidth}px`;
+document.querySelector(".main__explore").style.width = logoWidth;
+document.querySelector(".contact__time").style.width = logoWidth;
+document.querySelector(".rules__copyright").style.width = logoWidth;
+document
+  .querySelectorAll(".info__block-title")
+  .forEach((item) => (item.style.minWidth = logoWidth));
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger-menu");
+  const mobileMenu = document.querySelector(".mobile-menu-curtain");
+
+  burgerMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("mobile-menu--active");
+
+    this.classList.toggle("burger--active");
+  });
+});
+
+function adjustFontSize(element) {
+  const containerWidth = element.parentElement.offsetWidth;
+  let fontSize = 10; // Стартовый размер
+  element.style.fontSize = fontSize + "px";
+
+  while (
+    element.offsetWidth < document.body.clientWidth - 46 &&
+    fontSize < 1000
+  ) {
+    console.log(element.clientWidth, document.body.clientWidth);
+    fontSize++;
+    element.style.fontSize = fontSize + "px";
+  }
+}
+
+// Пример вызова для элемента с id="myText"
+setInterval(() =>
+  adjustFontSize(document.querySelector(".footer__title"), 1500)
+);
